e84be0e3a053444d9562e05ffc89af6d630ad218
mojfazel <fazeli.mojtaba@gmail.com>
2020-12-14 22:28:45 -0500

    updates db password

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php 
 try {
     $dsn = 'mysql:host=localhost;dbname=seussology';
-    $db = new pdo($dsn, 'root', '');
+    $db = new pdo($dsn, 'root', 'password');
 } catch(PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
